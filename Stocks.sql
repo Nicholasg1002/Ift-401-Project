2832537514207a33d41a756ad7eda8dffb9633b1
Nicholasg1002 <163094613+Nicholasg1002@users.noreply.github.com>
2024-04-20 17:20:06 -0700

    Update Stocks.sql

--- Stocks.sql
+++ Stocks.sql
@@ -7,9 +7,6 @@ price decimal(10,1) not null
 );
 
 
-insert into share (id,name,low,high,price) values (1,"ABCAPITAL",98.00,101.70,99.40);
-insert into share (id,name,low,high,price) values (2,"ADANIENT",206.60,212.35,210.40);
-insert into share (id,name,low,high,price) values (3,"ADANIGAS",151.40,154.45,151.90);
-insert into share (id,name,low,high,price) values (4,"ADANIGREEN",129.15,135.50,131.90);
-insert into share (id,name,low,high,price) values (5,"ADANIPORTS",370.90,376.50,372.50);
-insert into share (id,name,low,high,price) values (6,"ADANITRANS",326.00,345.40,335.95);
+insert into share (id,name,low,high,price) values (1,"SBUX",87.15,88.08,87.61);
+insert into share (id,name,low,high,price) values (2,"INTC",34.18,35.13,34.20);
+insert into share (id,name,low,high,price) values (3,"AMD",145.30,154.22,146.64);
